76fbc68609e831b75c0a24600b8f2a4f752a5a1c
Jules <julesbrulon@orange.fr>
2023-03-29 11:39:29 -0400

    #2 Premier jet IA

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyFollow : MonoBehaviour
 {
@@ -9,14 +10,7 @@ public class EnemyFollow : MonoBehaviour
     public float Hitpoints;
     public float maxHealth = 20;
 
-    [SerializeField] private float speed = 1;
-
-    private Transform target;
-
-    void Awake()
-    {
-        target = FindObjectOfType<Movement>().transform;
-    }
+    public AIPath aiPath;
 
     void Start()
     {
@@ -40,12 +34,20 @@ public class EnemyFollow : MonoBehaviour
             PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(damageOnCollision);
 
+            Destroy(transform.parent.gameObject);
             Destroy(gameObject);
         }
     }
 
-    private void Update()
+    public void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
